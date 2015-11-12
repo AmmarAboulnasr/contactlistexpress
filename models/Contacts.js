3583391738c33a11b2605c0ae9fdd8aa7ca4b35b
Ammar <ammar@ammaronline.net>
2015-11-12 00:28:31 -0800

    blueprint of the app

---
+++ models/Contacts.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Contacts = {};
+
+
+
+
+
+module.exports = Contacts;
+
+
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+
+var router = express.Router();
+
+var Contacts = require('../models/Contacts');
+
+
+
+
+
+module.exports = router;
